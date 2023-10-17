fcdbd144a2c92fb27f9aff75bbf34a2a233dca30
Poorav <118240170+poorav03@users.noreply.github.com>
2023-10-17 20:12:53 +0530

    Add files via upload

---
+++ Lecture8-I.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/*
+1.  Explained about the function.
+2.  Explained main function in the video using first program(hello world).
+3.  return type of the function.
+*/
+
+int main(){
+    cout << "Hello World";
+    return 0;
+}
